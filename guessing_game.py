35bb682e1d73fca10e8537ec3fbe918399daa05d
Samthesurf <ukpsamuel67@gmail.com>
2023-02-28 02:43:39 +0100

    added useful comments

--- guessing_game.py
+++ guessing_game.py
@@ -10,6 +10,7 @@ def clear():
 
 def number_game():
     '''This is a number guessing game function'''
+    print(logo2)
     print("Welcome to the number guessing game \nI am thinking of a number between 1 and 100")
     numbers = [x for x in range(1, 101)]
     computers_choice = rn.choice(numbers)
@@ -19,31 +20,33 @@ def number_game():
         your_choice = int(input("What's the number? "))
         if computers_choice == your_choice:
             print(f"Psst, the number is {computers_choice}")
-            return True
+            return True  # placeholder for when the guess actually becomes true aka equal to the computer's choice
         elif computers_choice > your_choice:
             print(f"Too low")
+            return False  #placeholder for when the guess is false, not necessary
         else:
             print(f'Too high')
-        return False
+            return False  
         
     def easy():
-        '''If difficulty is easy'''
+        '''If the difficulty is easy'''
         num_choices = 10
-        for i in range(num_choices):
-            print(f"You have {num_choices-i} chances left")
-            if guess():
+        while num_choices > 0:
+            print(f"You have {num_choices} chances left")
+            num_choices -= 1
+            if guess():  # if the guess function is true (return True), going back to the guess function, means the guessed number is equal to the computer's choice
                 print(f'Psst you guessed correctly, the random number was {computers_choice}')
                 return
         print('Game over, you lost')
             
     def hard():
-        '''If difficulty is hard'''
+        '''If the difficulty is hard'''
         num_choices = 5
         for i in range(num_choices):
-            print(f"You have {num_choices-i} chances left")
+            print(f"You have {num_choices-i} chances left")  # it does 5-1,5-2,5-3, etc until it does it 5 times, which equals 0 chances left
             if guess():
                 print(f'Psst you guessed correctly, the random number was {computers_choice}')
-                return
+                return  # ending the loop
         print('Game over, you lost')
 
     while True:
@@ -61,7 +64,7 @@ def number_game():
     while True: 
         if input("Do you want to restart the game? Type 'y' for  yes and 'n' for no: ") == 'y':
             clear()
-            number_game()
+            number_game() # absolutely just copied this from my blackjack game, because recursion seems cool to me
         else: 
             print('Game over')
             break
